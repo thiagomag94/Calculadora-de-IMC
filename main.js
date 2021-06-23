ec8f22ec009a8d193e2695b6ca14917b0a83aff7
thiagomag94 <thiagomag94@gmail.com>
2021-06-22 22:08:38 -0300

    atualizando página

--- main.js
+++ main.js
@@ -1,44 +1,52 @@
 function imc() {
     var peso = document.getElementById('peso').value
     var alt = document.getElementById('alt').value
+    const resultado = document.getElementById('resultado-titulo')
     
     a = window.document.getElementById("resultado")
     var imc = (peso/(alt**2)).toFixed(2)
 
-    if (imc < 18.5) 
-
-              a.innerHTML = `<div class="alert alert-primary" role="alert">
-              O seu IMC é ${imc} - Abaixo do peso
-            </div>`
-    
-    if (imc > 18.5 & imc < 24.9) 
-
-          a.innerHTML = `<div class="alert alert-primary" role="alert">
-          O seu IMC é ${imc} - Peso normal
-        </div>`
-
-    if (imc > 25 & imc < 29.9) 
-
-        a.innerHTML = `<div class="alert alert-primary" role="alert">
+    if (imc < 18.5) {
+              resultado.style.display = "block"
+              a.innerHTML = `<div class="alert alert-danger" role="alert">
+                             O seu IMC é ${imc} - Abaixo do peso
+                             </div>`
+    }
+    if (imc >= 18.5 & imc <= 24.9){ 
+          resultado.style.display = "block"
+          a.innerHTML = `
+                          <div class="alert alert-success" role="alert">
+                          O seu IMC é ${imc} - Peso normal
+                          </div><h5>Parabéns, seu IMC é ${imc} kg/m² e você está dentro do peso adequado!</h5>`
+    }
+    if (imc >= 25 & imc <= 29.9) {
+        resultado.style.display = "block"
+        a.innerHTML = `<div class="alert alert-warning" role="alert">
         O seu IMC é ${imc} - Sobrepeso
-      </div>`
-
-    if (imc > 30 & imc < 34.9) 
-
-      a.innerHTML = `<div class="alert alert-primary" role="alert">
+      </div><h5>Infelizmente, seu IMC é ${imc} kg/m² e você está fora do peso adequado!</h5>`
+    }
+    if (imc >= 30 & imc <= 34.9) {
+      resultado.style.display = "block"
+      a.innerHTML = `<div class="alert alert-danger" role="alert">
       O seu IMC é ${imc} - Obesidade grau 1
-    </div>`
+    </div><h5>Infelizmente, seu IMC é ${imc} kg/m² e você está fora do peso adequado!</h5>`
+    }
 
-    if (imc > 35 & imc < 39.9) 
-
-        a.innerHTML = `<div class="alert alert-primary" role="alert">
+    if (imc >= 35 & imc <= 39.9) {
+        resultado.style.display = "block"
+        a.innerHTML = `<div class="alert alert-danger" role="alert">
         O seu IMC é ${imc} - Obesidade grau 2
-      </div>`
-
-    if (imc >= 40) 
-
-      a.innerHTML = `<div class="alert alert-primary" role="alert">
+      </div><h5>Infelizmente, seu IMC é ${imc} kg/m² e você tem um grau de  obesidade!</h5>`
+     }
+    if (imc >= 40) {
+      resultado.style.display = "block"
+      a.innerHTML = `<div class="alert alert-danger" role="alert">
       O seu IMC é ${imc} - Obesidade mórbida
     </div>`
+    }
     
+  }
+
+  function refresh () {
+    document.location.reload(true)
   }
